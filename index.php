9f2871003611283b6660a96f1b0a2cccb329caa3
dinashofiafasyadilla <93061365+dinashofiafasyadilla@users.noreply.github.com>
2021-11-19 11:41:22 +0700

    menambahkan index.php

--- index.php
+++ index.php
@@ -1,65 +1,195 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-class ModelBuku extends CI_Model
-{
- //manajemen buku
- public function getBuku()
- {
- return $this->db->get('buku');
- }
- public function bukuWhere($where)
- {
- return $this->db->get_where('buku', $where);
- }
- public function simpanBuku($data = null)
- {
- $this->db->insert('buku',$data);
- }
- public function updateBuku($data = null, $where = null)
- {
- $this->db->update('buku', $data, $where);
- }
- public function hapusBuku($where = null)
- {
- $this->db->delete('buku', $where);
- }
- public function total($field, $where)
- {
- $this->db->select_sum($field);
- if(!empty($where) && count($where) > 0){
- $this->db->where($where);
- }
- return $this->db->get()->row($field);
-  }
- //manajemen kategori
-  public function getKategori()
-   {
-    return $this->db->get('kategori');
-     }
-      public function kategoriWhere($where)
-       {
-        return $this->db->get_where('kategori', $where);
-         }
-          public function simpanKategori($data = null)
-           {
-            $this->db->insert('kategori', $data);
-             }
-              public function hapusKategori($where = null)
-               {
-                $this->db->delete('kategori', $where);
-                 }
-                  public function updateKategori($where = null, $data = null)
-                   {
-                    $this->db->update('kategori', $data, $where);
-                     }
-                      //join
-                       public function joinKategoriBuku($where)
-                        {
-                         $this->db->select('buku.id_kategori,kategori.kategori');
-                          $this->db->from('buku');
-                           $this->db->join('kategori','kategori.id = 
-                           buku.id_kategori');
-                            $this->db->where($where);
-                             return $this->db->get();
-                              }
-                              }
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <!-- row ux -->
+    <div class="row">
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-danger shadow h-100 py-2 bg-primary">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-md font-weight-bold text-white text-uppercase mb-1">Jumlah Anggota</div>
+                            <div class="h1 mb-0 font-weight-bold text-white"><?= $this->ModelUser->getUserWhere(['role_id' => 1])->num_rows(); ?></div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="<?= base_url('user/anggota'); ?>"><i class="fas fa-users fa-3x text-warning"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2 bg-warning">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-md font-weight-bold text-white text-uppercase mb-1">Stok Buku Terdaftar</div>
+                            <div class="h1 mb-0 font-weight-bold text-white">
+                                <?php
+                                $where = ['stok != 0'];
+                                $totalstok = $this->ModelBuku->total('stok', $where);
+                                echo $totalstok;        
+                                ?>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="<?= base_url('buku'); ?>"><i class="fas fa-book fa-3x text-primary"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2 bg-danger">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-md font-weight-bold text-white text-uppercase mb-1">Buku yang dipinjam</div>
+                            <div class="h1 mb-0 font-weight-bold text-white">
+                                <?php
+                                $where = ['pinjam != 0'];
+                                $totaldipinjam = $this->ModelBuku->total('dipinjam', $where);
+                                echo $totaldipinjam;
+                                ?>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="<?= base_url('user'); ?>"><i class="fas fa-user-tag fa-3x text-success"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2 bg-danger">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-md font-weight-bold text-white text-uppercase mb-1">Buku yang dipinjam</div>
+                            <div class="h1 mb-0 font-weight-bold text-white">
+                                <?php
+                                $where = ['dipinjam != 0'];
+                                $totaldipinjam = $this->ModelBuku->total('dipinjam', $where);
+                                echo $totaldipinjam;
+                                ?>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="<?= base_url('user'); ?>"><i class="fas fa-user-tag fa-3x text-success"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2 bg-success">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-md font-weight-bold text-white text-uppercase mb-1">Buku yang dibooking</div>
+                            <div class="h1 mb-0 font-weight-bold text-white">
+                                <?php
+                                $where = ['dibooking != 0'];
+                                $totaldibooking = $this->ModelBuku->total('dibooking', $where);
+                                echo $totaldibooking;
+                                ?>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="<?= base_url('user'); ?>"><i class="fas fa-shopping-cart fa-3x text-success"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end row ux -->
+
+    <!-- divider -->
+    <hr class="sidebar-divider">
+
+    <!-- row table -->
+    <div class="row">
+        <div class="table-responsive table-bordered col-sm-5 ml-auto mr-auto mt-2">
+            <div class="page-header">
+                <span class="fas fa-users text-primary mt-2">Data User</span>
+                <a class="text-danger" href="<?= base_url('user/data_user'); ?>"><i class="fas fa-search mt-2 float-right">Tampilkan</i></a>
+            </div>
+            <table class="table mt-3">
+                <thead class="table mt-3">
+                    <tr>
+                        <th>#</th>
+                        <th>Nama Anggota</th>
+                        <th>Email</th>
+                        <th>Role ID</th>
+                        <th>Aktif</th>
+                        <th>Member Sejak</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $i = 1;
+                    foreach ($anggota as $a) :
+                    ?>
+                    <tr>
+                        <td><?= $i++; ?></td>
+                        <td><?= $a['nama']; ?></td>
+                        <td><?= $a['email']; ?></td>
+                        <td><?= $a['role_id']; ?></td>
+                        <td><?= $a['is_active']; ?></td>
+                        <td><?= $a['tanggal_input']; ?></td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="table-responsive table-bordered col-sm-5 ml-auto mr-auto mt-2">
+            <div class="page-header">
+                <span class="fas fa-book text-warning mt-2">Data Buku</span>
+                <a href="<?= base_url('user/data_user'); ?>"><i class="fas fa-search text-primary mt-2 float-right">Tampilkan</i></a>
+            </div>
+            <div class="table-responsive">
+                <table class="table mt-3" id="table-datatable">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Judul Buku</th>
+                            <th>Pengarang</th>
+                            <th>Penerbit</th>
+                            <th>Tahun Terbit</th>
+                            <th>ISBN</th>
+                            <th>Stok</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1;
+                        foreach ($buku as $b) :
+                        ?>
+                        <tr>
+                            <td><?= $i++; ?></td>
+                            <td><?= $b['judul_buku']; ?></td>
+                            <td><?= $b['pengarang']; ?></td>
+                            <td><?= $b['penerbit']; ?></td>
+                            <td><?= $b['tahun_terbit']; ?></td>
+                            <td><?= $b['isbn']; ?></td>
+                            <td><?= $b['stok']; ?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
+    </div>
+    <!-- end of row table -->
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- end of main content -->
